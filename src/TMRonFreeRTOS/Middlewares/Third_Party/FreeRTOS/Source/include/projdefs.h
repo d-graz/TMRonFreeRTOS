e670fef17181b9083eae017665642ccc1c169189
Davide Grazzani <davide.grazzani@mail.polimi.it>
2024-04-29 13:17:35 +0200

    Changed structure of TCB_t in order to have more clean redundant config

--- src/TMRonFreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+++ src/TMRonFreeRTOS/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
@@ -118,5 +118,29 @@ typedef void (* TaskFunction_t)( void * );
 #define pdLITTLE_ENDIAN                   pdFREERTOS_LITTLE_ENDIAN
 #define pdBIG_ENDIAN                      pdFREERTOS_BIG_ENDIAN
 
+#if ( configUSE_REDUNDANT_TASK == 1)
+    typedef struct xRedundantShared {
+        BaseType_t isRedundantTask;                    /*< The type of the task. */
+        void (*pxCommitFunction)(void*);               /*< Used for the commit function of the task. */
+        void *pxCommitFunctionParameter;               /*< Used for the parameter of the commit function of the task. */
+        void * pxPreviousInputStruct;                  /*< Input at t-1 for task. */
+        UBaseType_t uInputStructSize;                  /*< Used for the size of the input structure of the task. */
+        UBaseType_t uOutputStructSize;                 /*< Used for the size of the output structure of the task. */
+        BaseType_t isRecoveryProcess;                  /*< Used for the recovery process of the task. */
+        TaskFunction_t taskCode;                       /*< Used for the task code of the task. */
+        uint32_t stackDepth;                           /*< Used for the stack depth of the task. */
+        void * pvParameters;                           /*< Used for the parameters of the task. */
+    } xRedundantShared_t;
+
+    typedef struct xRedundantStruct {
+        xRedundantShared_t * pxRedundantShared;        /*< Used for the shared structure of the task. */
+        struct tskTaskControlBlock * pxTaskValidation; /*< Used for validation of the TCB. */
+        struct tskTaskControlBlock * pxTaskSUS;        /*< Used for error correction of the TCB in case of divergent results in the validation task. */
+        uint64_t iterationCounter;                     /*< Used for controlling the execution status of the task. */
+        void * pxInputStruct;                          /*< Used for the input structure of the task. */
+        void * pxOutputStruct;                         /*< Used for the output structure of the task. */
+    } xRedundantStruct_t;
+
+#endif
 
 #endif /* PROJDEFS_H */
